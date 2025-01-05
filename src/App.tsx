d1633643047503aedf3a7223272e397fb2cce94b
Kazuya-Sakashita <kz0508@gmail.com>
2025-01-05 16:51:33 +0900

    カウント数表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // 状態変化をモニタリングしてコンソールに表示
+  useEffect(() => {
+    console.log(`Count value updated: ${count}`);
+  }, [count]); // count が変更されるたびに実行
 
   return (
     <>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
